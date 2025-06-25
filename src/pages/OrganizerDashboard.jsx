92c14fcb0b817f0b3e3cd9f350da144f443f8270
Danishwer _ <dannywits12321@gmail.com>
2025-06-25 17:27:17 +0530

    import fix

--- src/pages/OrganizerDashboard.jsx
+++ src/pages/OrganizerDashboard.jsx
@@ -1,14 +1,14 @@
 import { Title } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import { Navigate } from "react-router";
-import PageLoader from "../components/pageLoader";
+import PageLoader from "../components/PageLoader";
 import { checkOrganizer } from "../utils/databaseHelper";
 import { routes } from "../utils/routes";
 import useSupabase from "../utils/supabaseHook";
 import WebFrame from "../WebFrame";
 
 function OrganizerDashboard() {
-  const { user, logout } = useSupabase();
+  const { user } = useSupabase();
   const { data: isOrganizer, isLoading } = useQuery({
     queryKey: ["organizer"],
     queryFn: () => checkOrganizer(user?.id),
