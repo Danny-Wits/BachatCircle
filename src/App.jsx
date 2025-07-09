26194bd3b3770b4a1333a75e3dc38c0f48379c4f
Danishwer _ <dannywits12321@gmail.com>
2025-07-09 16:56:21 +0530

    color of login page fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { Center } from "@mantine/core";
+import { Center, useComputedColorScheme } from "@mantine/core";
 import Auth from "./Auth";
 import Home from "./Home";
 import useSupabase from "./utils/supabaseHook";
 function App() {
   const { isAuthenticated, invite } = useSupabase();
+  const computedColorScheme = useComputedColorScheme();
   if (!isAuthenticated)
     return (
       <Center
         h="100vh"
         style={{
-          background: "linear-gradient(45deg, #FFA500 45%, #FFFFFF 45%)",
+          background: `linear-gradient(45deg, #FFA500 45%, ${
+            computedColorScheme === "dark" ? "#333" : "#fff"
+          } 45%)`,
           backgroundSize: "120% 120%",
           animation: "wave-gradient 6s ease-in-out infinite",
         }}
